280709f1bbc8edf5b8a56bd4d24715cde5a54580
isfturtle <singingturtle@gmail.com>
2018-06-20 10:20:34 -0400

    ready to try react stuff on heroku

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
-    renderContent() {
-        if (!this.props.auth) {
-            return <li><a href="/auth/google">Login With Google</a></li>;
-        } else {
-            return <li><a>Logout</a></li>;
-        }
-
+  renderContent() {
+    console.log(this.props.auth);
+    switch (this.props.auth) {
+      case null:
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
+  }
 
-    render() {
-        return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a className="left brand-logo">
-                        Emaily
-          </a>
-                    <ul className="right">
-                        {this.renderContent()}
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            FeedbackLoop
+          </Link>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 function mapStateToProps({ auth }) {
-    return { auth };
+  return { auth };
 }
 
 export default connect(mapStateToProps)(Header);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
+const bodyParser = require("body-parser");
 const passport = require('passport')
 const keys = require('./config/keys')
 require('./models/User')
@@ -18,8 +19,23 @@ app.use(
 )
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(bodyParser.json());
 
-require('./routes/authRoutes')(app)
+require('./routes/authRoutes')(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
